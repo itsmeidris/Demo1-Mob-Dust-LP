ecc2aa609e818f8d9383a1e0c8d421a991479259
itsmeidris <99984030+itsmeidris@users.noreply.github.com>
2022-08-31 15:17:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+gsap.from("header" ,{duration :2, y:"-150px",ease:"power1" });
+
+/*Section 1 Gsap*/
+gsap.from(".main1h3" ,{duration:1 ,x:"-400px" ,opacity: 0 ,ease:"power1"})
+gsap.from(".main1h1" ,{duration:2 ,x:"-400px" ,opacity: 0 ,ease:"power1"});
+gsap.from(".main1p" ,{duration:3 ,x:"-400px" ,opacity: 0 ,ease:"power1"});
+gsap.from(".start" ,{duration:4 ,x:"-400px" ,opacity: 0 ,ease:"power1"});
+
+
+/*Section 2 Gsap*/
+gsap.from(".main2h3" ,{duration:1 ,x:"400px" ,opacity: 0 ,ease:"power1",
+scrollTrigger :".main2h3" ,x:400});
+
+gsap.from(".main2h1" ,{duration:2 ,x:"400px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".main2h1" ,x:400});
+
+gsap.from(".main2p" ,{duration:3 ,x:"400px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger: ".main2p" ,x:400});
+gsap.from(".main2boxes" ,{duration:3 ,x:"400px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".main2boxes" ,x:400});
+
+gsap.from(".aside1" ,{duration:4 ,x:"-400px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".aside1" ,x:-400});
+
+
+/*Section 3 Gsap*/
+gsap.from(".main3h3" ,{duration:1 ,x:"400px" ,opacity: 0 ,ease:"power1",
+scrollTrigger :".main3h3" ,x:400});
+
+gsap.from(".main3h1" ,{duration:2 ,x:"400px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".main3h1" ,x:400});
+
+gsap.from(".main3p" ,{duration:3 ,x:"400px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger: ".main3p" ,x:400});
+
+gsap.from(".box1X" ,{duration:4 ,x:"-700px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".box1X" ,x:-700});
+gsap.from(".box2X" ,{duration:3 ,x:"-650px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".box2X" ,x:-650});
+gsap.from(".box3X" ,{duration:2 ,x:"-600px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".box3X" ,x:-600});
+
+gsap.from(".box1X1" ,{duration:4 ,x:"700px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".box1X1" ,x:700});
+gsap.from(".box2X1" ,{duration:3 ,x:"650px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".box2X1" ,x:650});
+gsap.from(".box3X1" ,{duration:2 ,x:"600px" ,opacity: 0 ,ease:"power1"
+,scrollTrigger :".box3X1" ,x:600});
+
+
